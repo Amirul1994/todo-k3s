429c7f4a84152c120be4f8d92c5c640d934b5a85
amirul1994 <amirulbrinto90@gmail.com>
2024-08-17 15:29:05 +0000

    added todo-service

--- todo-service/index.js
+++ todo-service/index.js
@@ -5,7 +5,7 @@ const todocontroller = require('./todocontroller')
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 app.use(express.json());
 
